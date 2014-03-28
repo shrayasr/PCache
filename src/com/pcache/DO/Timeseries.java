98c7c070541e322fa05bddd64ffd6f8418d9b9f9
Shrayas <shrayasr@gmail.com>
2014-03-28 23:48:34 +0530

    Moving to a TreeMap based approach
    TreeMaps make some sense. The Key would be a LONG type, representing the
    no. of miliseconds from epoc and the Value would be an Object of any
    type
    
    The timeseries class is now templated. Which means that it can store
    objects of any type

--- src/com/pcache/DO/Timeseries.java
+++ src/com/pcache/DO/Timeseries.java
@@ -2,40 +2,47 @@ package com.pcache.DO;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.TreeMap;
+
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.ISODateTimeFormat;
 
 import com.pcache.exceptions.PCacheException;
 
 /**
  * Data Object to store the list of timeseries in the cache
- * @author shrayas
- *
  */
-public class Timeseries {
+public class Timeseries<T> {
 
-	private ArrayList<Date> _timestamps;
-	private ArrayList<Object> _dataPoints;
+	private TreeMap<Long, T> _timeseries;
 
 	public int size() {
-		return this._timestamps.size();
+		return this._timeseries.size();
 	}
-
-	/**
-	 * Constructor. Initialize a time series 
-	 * @param timestamps the list of timestamps
-	 * @param dataPoints the associated list of data points 
-	 * @throws PCacheException thrown if different length arrays are provided
-	 * 			for timestamps and dataPoints
-	 */
-	public Timeseries (ArrayList<Date> timestamps, 
-			ArrayList<Object> dataPoints) throws PCacheException {
-
-		if (timestamps.size() != dataPoints.size()) {
-			throw new PCacheException("Timeseries data not correlated with"
-					+ " data points provided. (Should be same length)");
+	
+	public Timeseries (ArrayList<String> timestampsISO8601, 
+			ArrayList<T> dataPoints) throws PCacheException {
+		
+		_timeseries = new TreeMap<Long, T>();
+		
+		if (timestampsISO8601.size() != dataPoints.size()) {
+			throw new PCacheException("Sizes don't match. The number of data " +
+					"points should equal the number of timestamps");
 		}
 		
-		this._timestamps = timestamps;
-		this._dataPoints = dataPoints;
+		DateTimeFormatter formatter = ISODateTimeFormat.dateTime();
+		
+		for (int i=0; i<timestampsISO8601.size(); i++) {
+			
+			String timestampISO8601 = timestampsISO8601.get(i);
+			T dataPoint = dataPoints.get(i);
+			
+			long milisSinceEpoc = formatter.parseDateTime(timestampISO8601)
+					.getMillis();
+			
+			_timeseries.put(milisSinceEpoc, dataPoint);
+			
+		}
 		
 	}
 
