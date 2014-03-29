6cfda04fb786ed4e7a1f91777fbe7310d2bc53ff
Shrayas <shrayasr@gmail.com>
2014-03-29 23:30:15 +0530

    Adding more timeseries shiz to the class
    Adding functions like rangeBetween and rangeFrom that allows one to get
    the required part of a timeseries
    
    Also adding in some test cases
    
    TODO
      comments.

--- src/com/pcache/DO/Timeseries.java
+++ src/com/pcache/DO/Timeseries.java
@@ -1,7 +1,7 @@
 package com.pcache.DO;
 
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.Map;
 import java.util.TreeMap;
 
 import org.joda.time.format.DateTimeFormatter;
@@ -14,27 +14,71 @@ import com.pcache.exceptions.PCacheException;
  */
 public class Timeseries<T> {
 
-	private TreeMap<Long, T> _timeseries;
+	private Map<Long, T> _timeseries;
 
 	public int size() {
 		return this._timeseries.size();
 	}
 	
-	public Timeseries (ArrayList<String> timestampsISO8601, 
+	public Map<Long, T> rangeBetween(String timestampFrom, 
+			String timestampTo) {
+		
+		DateTimeFormatter formatter = ISODateTimeFormat.dateTime();
+		
+		long milisSinceEpocFrom = formatter.parseDateTime(timestampFrom)
+				.getMillis();
+
+		long milisSinceEpocTo = formatter.parseDateTime(timestampTo)
+				.getMillis();
+
+		return ((TreeMap<Long, T>) this._timeseries).subMap(milisSinceEpocFrom, 
+				true, milisSinceEpocTo, true);
+
+	}
+
+	public Map<Long, T> rangeFrom(String timestampFrom) {
+
+		DateTimeFormatter formatter = ISODateTimeFormat.dateTime();
+
+		long milisSinceEpocFrom = formatter.parseDateTime(timestampFrom)
+				.getMillis();
+
+		long lastKey = ((TreeMap<Long, T>) this._timeseries).lastKey();
+
+		return ((TreeMap<Long, T>) this._timeseries).subMap(milisSinceEpocFrom, 
+				true, lastKey, true);
+		
+	}
+
+	public Map<Long, T> rangeTo(String timestampTo) {
+
+		DateTimeFormatter formatter = ISODateTimeFormat.dateTime();
+
+		long milisSinceEpocTo = formatter.parseDateTime(timestampTo)
+				.getMillis();
+
+		long firstKey = ((TreeMap<Long, T>) this._timeseries).firstKey();
+
+		return ((TreeMap<Long, T>) this._timeseries).subMap(firstKey, 
+				true, milisSinceEpocTo, true);
+		
+	}
+	
+	public Timeseries (ArrayList<String> timestamps, 
 			ArrayList<T> dataPoints) throws PCacheException {
 		
 		_timeseries = new TreeMap<Long, T>();
 		
-		if (timestampsISO8601.size() != dataPoints.size()) {
+		if (timestamps.size() != dataPoints.size()) {
 			throw new PCacheException("Sizes don't match. The number of data " +
 					"points should equal the number of timestamps");
 		}
 		
 		DateTimeFormatter formatter = ISODateTimeFormat.dateTime();
 		
-		for (int i=0; i<timestampsISO8601.size(); i++) {
+		for (int i=0; i<timestamps.size(); i++) {
 			
-			String timestampISO8601 = timestampsISO8601.get(i);
+			String timestampISO8601 = timestamps.get(i);
 			T dataPoint = dataPoints.get(i);
 			
 			long milisSinceEpoc = formatter.parseDateTime(timestampISO8601)
--- src/com/pcache/tests/TimeseriesTest.java
+++ src/com/pcache/tests/TimeseriesTest.java
@@ -3,6 +3,9 @@ package com.pcache.tests;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.junit.After;
 import org.junit.Before;
@@ -24,9 +27,6 @@ public class TimeseriesTest
 			
 		}};
 		
-		System.out.println(timestamps.size());
-		System.out.println(timestamps.get(0));
-		
 		ArrayList<String> dataPoints = new ArrayList<String>() {{
 			
 			add("UP");
@@ -35,6 +35,128 @@ public class TimeseriesTest
 		
 		Timeseries<String> ts = new Timeseries<String>(timestamps, dataPoints);
 		
+	}
+
+	@Test
+	public void testTimeseriesRangeBetween() throws PCacheException
+	{
+		
+		ArrayList<String> timestamps = new ArrayList<String>() {{
+			
+			add("2010-01-01T12:00:00.000+05:30");
+			add("2010-01-02T12:00:00.000+05:30");
+			add("2010-01-03T12:00:00.000+05:30");
+			add("2010-01-08T12:00:00.000+05:30");
+			add("2010-01-10T12:00:00.000+05:30");
+			add("2010-01-11T12:00:00.000+05:30");
+			add("2010-01-23T12:00:00.000+05:30");
+			add("2010-01-27T12:00:00.000+05:30");
+			
+		}};
+		
+		ArrayList<String> dataPoints = new ArrayList<String>() {{
+			
+			add("UP");
+			add("DOWN");
+			add("UP");
+			add("UP");
+			add("DOWN");
+			add("UP");
+			add("UP");
+			add("UP");
+			
+		}};
+
+		Timeseries<String> ts = new Timeseries<String>(timestamps, dataPoints);
+
+		String timestampFrom = "2010-01-05T12:00:00.000+05:30";
+		String timestampTo = "2010-01-20T12:00:00.000+05:30";
+		
+
+		Map<Long, String> tsSub = ts.rangeBetween(timestampFrom, timestampTo);
+
+		assertEquals(3, tsSub.size());
+
+	}
+
+	@Test
+	public void testTimeseriesRangeFrom() throws PCacheException
+	{
+		
+		ArrayList<String> timestamps = new ArrayList<String>() {{
+			
+			add("2010-01-01T12:00:00.000+05:30");
+			add("2010-01-02T12:00:00.000+05:30");
+			add("2010-01-03T12:00:00.000+05:30");
+			add("2010-01-08T12:00:00.000+05:30");
+			add("2010-01-10T12:00:00.000+05:30");
+			add("2010-01-11T12:00:00.000+05:30");
+			add("2010-01-23T12:00:00.000+05:30");
+			add("2010-01-27T12:00:00.000+05:30");
+			
+		}};
+		
+		ArrayList<String> dataPoints = new ArrayList<String>() {{
+			
+			add("UP");
+			add("DOWN");
+			add("UP");
+			add("UP");
+			add("DOWN");
+			add("UP");
+			add("UP");
+			add("UP");
+			
+		}};
+
+		Timeseries<String> ts = new Timeseries<String>(timestamps, dataPoints);
+
+		String timestampFrom = "2010-01-05T12:00:00.000+05:30";
+
+		Map<Long, String> tsSub = ts.rangeFrom(timestampFrom);
+
+		assertEquals(5, tsSub.size());
+
+	}
+
+	@Test
+	public void testTimeseriesRangeTo() throws PCacheException
+	{
+		
+		ArrayList<String> timestamps = new ArrayList<String>() {{
+			
+			add("2010-01-01T12:00:00.000+05:30");
+			add("2010-01-02T12:00:00.000+05:30");
+			add("2010-01-03T12:00:00.000+05:30");
+			add("2010-01-08T12:00:00.000+05:30");
+			add("2010-01-10T12:00:00.000+05:30");
+			add("2010-01-11T12:00:00.000+05:30");
+			add("2010-01-23T12:00:00.000+05:30");
+			add("2010-01-27T12:00:00.000+05:30");
+			
+		}};
+		
+		ArrayList<String> dataPoints = new ArrayList<String>() {{
+			
+			add("UP");
+			add("DOWN");
+			add("UP");
+			add("UP");
+			add("DOWN");
+			add("UP");
+			add("UP");
+			add("UP");
+			
+		}};
+
+		Timeseries<String> ts = new Timeseries<String>(timestamps, dataPoints);
+
+		String timestampTo = "2010-01-20T12:00:00.000+05:30";
+		
+		Map<Long, String> tsSub = ts.rangeTo(timestampTo);
+
+		assertEquals(6, tsSub.size());
+
 	}
 
 }
