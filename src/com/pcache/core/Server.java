1b5a9900357d485234dcaacfaae0240a265a4ca1
Shrayas <shrayasr@gmail.com>
2014-06-15 00:31:43 +0530

    A server is supposed to handle multiple conns, doh
    * Dunno why git isn't treating this as a rename, meh.

---
+++ src/com/pcache/core/Server.java
@@ -0,0 +1,30 @@
+package com.pcache.core;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.sql.PooledConnection;
+
+public class Server {
+
+	public static void main (String[] args) {
+		
+		ExecutorService executorService = Executors.newFixedThreadPool(10);
+		
+		int PORT_NUMBER=6369;
+		
+		System.out.println("Listening on " + PORT_NUMBER);
+
+		boolean listening = true;
+		try (ServerSocket serverSocket = new ServerSocket(PORT_NUMBER)) {
+			
+			while (listening) {
+				executorService.execute(new RequestHandler(serverSocket.accept()));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
