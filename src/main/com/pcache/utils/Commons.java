a23ec37b51e81e96b1292278eba3708c6f1c2fbd
Shrayas <shrayasr@gmail.com>
2014-06-22 13:50:45 +0530

    Removing unnecessary functions from commons
    * These functions were useful when i had the fixed timeseries. That
      boomed, this also booms.

--- src/main/com/pcache/utils/Commons.java
+++ src/main/com/pcache/utils/Commons.java
@@ -60,71 +60,6 @@ public class Commons
 
 	}
 	
-	/**
-	 * Parse a tick string and return the no. of miliseconds in that tick.
-	 * Eg: 1D is 1 Day. 1 Day contains 86400000 miliseconds.
-	 * 
-	 * @param tickStr the string representing the no. of ticks between timestamps
-	 * 			in the array. 
-	 * 
-	 * 			The following units are acceptable:
-	 * 				d:  Day,
-	 * 				m:  Month,
-	 * 				y:  Year,
-	 * 				h:  Hour,
-	 * 				M:  Minute,
-	 * 				s:  Second.
-	 * 
-	 * @return no. of miliseconds in the tick string.
-	 * @throws PCacheException 
-	 */
-	public static long parseTickString(String tickStr) throws PCacheException {
-			
-		Pattern pattern = Pattern.compile("([0-9]+)([dmyHMs])$");
-		Matcher matcher = pattern.matcher(tickStr);
-		
-		int tickDuration = 1;
-		String tickUnit = "d";
-		
-		if (matcher.groupCount() != 2) {
-			throw new PCacheException("Invalid format for tick");
-		}
-		
-		if (matcher.matches()) {
-			tickDuration = Integer.parseInt(matcher.group(1));
-			tickUnit = matcher.group(2);
-		}
-		
-		if (tickUnit.equals("m") || tickUnit.equals("y")) {
-			throw new PCacheException("Tick value in months or years not " +
-					"supported yet");
-		}
-		
-		long milisInSecond = 1000;
-		long milisInMinute = milisInSecond * 60;
-		long milisInHour = milisInMinute * 60;
-		long milisInDay = milisInHour * 24;
-		
-		switch(tickUnit) {
-		
-		case "s":
-			return (tickDuration * milisInSecond);
-			
-		case "M":
-			return (tickDuration * milisInMinute);
-			
-		case "h":
-			return (tickDuration * milisInHour);
-			
-		case "d":
-			return (tickDuration * milisInDay);
-			
-		default:
-			throw new PCacheException("Tick format not supported");
-		
-		}
-	}
-	
 	public static int safeLongToInt(long l) {
 	    if (l < Integer.MIN_VALUE || l > Integer.MAX_VALUE) {
 	        throw new IllegalArgumentException
@@ -133,27 +68,4 @@ public class Commons
 	    return (int) l;
 	}
 	
-	/**
-	 * Calculate the index to offset. 
-	 * 
-	 * It is calculated as (timestamp - starting_timestamp) / tick 
-	 * 
-	 * Eg:
-	 * 		starting_timestamp = Jan 1 2013 = 1356998400
-	 * 		timestamp = Jan 5 2014 = 1357344000
-	 * 		tick = 1d = no. of milis in a day = 86400
-	 * 
-	 * 		Index of Jan 5th = (1357430400 - 1356998400) / 86400
-	 * 						 = 4
-	 * 
-	 * @param timestamp the timestamp to calculate the offset for
-	 * @param startime_timestamp the starting timestamp in the series
-	 * @param tick the tick value between timestamps
-	 * @return the offset into the array for that timestamp
-	 */
-	public static int getOffset(long timestamp, long startime_timestamp, 
-			long tick) {
-		
-		return (safeLongToInt((timestamp - startime_timestamp) / tick));
-	}
 }
